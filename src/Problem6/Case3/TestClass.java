aea21bdeadd51eaa7a367db8f62fcf161e4737f3
Hiago Dias <51371403+HiagoDiaaas@users.noreply.github.com>
2024-06-04 07:40:26 -0500

    Case 3

---
+++ src/Problem6/Case3/TestClass.java
@@ -0,0 +1,18 @@
+package Problem6.Case3;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Bird bird1 = new Bird("Arara", "Blue");
+        Bird bird2 = new Bird("Arara", "Blue");
+        Bird bird3 = new Bird("Canarinho", "Yellow");
+
+        System.out.println("bird1.equals(bird2): " + bird1.equals(bird2));
+        System.out.println("bird2.equals(bird3): " + bird2.equals(bird3));
+
+        BirdWithSound birdWithSound1 = new BirdWithSound("Arara", "Blue", "Squawk");
+        BirdWithSound birdWithSound2 = new BirdWithSound("Arara", "Blue", "Squawk");
+
+        System.out.println("birdWithSound1.equals(birdWithSound2): " + birdWithSound1.equals(birdWithSound2));
+        System.out.println("birdWithSound1.equals(bird1): " + birdWithSound1.equals(bird1));
+    }
+}
